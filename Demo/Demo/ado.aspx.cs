0ffb59b2547c9baba938138159f11f0a19c57123
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-11 17:16:17 +0530

    Fist Web page using ASP.net

---
+++ Demo/Demo/ado.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Demo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnClick(object sender, EventArgs e)
+        {
+            string cnstr = "data source=SAIKAT-SANIL\\SQL2019; initial catalog=B15; integrated security=true;";
+            SqlConnection cn = new SqlConnection(cnstr);
+            cn.Open();
+            lblResult.Text = "Successfully Connected";
+            cn.Close();
+        }
+    }
+}
