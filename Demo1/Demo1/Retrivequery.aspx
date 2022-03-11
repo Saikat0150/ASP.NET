29604da2efb60be0f70888bb6fc0a040066d5d65
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-11 18:01:09 +0530

    Retrive using select query

---
+++ Demo1/Demo1/Retrivequery.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Retrivequery.aspx.cs" Inherits="Demo1.Retrivequery" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="l1Data" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Demo1/Demo1/Retrivequery.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Demo1
+{
+    public partial class Retrivequery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string cnstr = "data source=SAIKAT-SANIL\\SQL2019;initial catalog=B15;integrated security=true;";
+            SqlConnection cn = new SqlConnection(cnstr);
+            string sqlstr = "select * from student";
+            SqlCommand cmd = new SqlCommand(sqlstr, cn);
+            SqlDataReader dr;
+            cn.Open();
+            l1Data.Text = "Database Connected.";
+            
+            dr = cmd.ExecuteReader();
+            while (dr.Read() == true)
+            {
+                l1Data.Text = l1Data.Text + dr.GetValue(0) + " " + dr.GetValue(1) + " " + dr.GetValue(2)+ " " + dr.GetValue(3);
+            }
+            dr.Close();
+            cn.Close();
+            l1Data.Text = l1Data.Text + "\n Database Disconnected";
+
+        }
+    }
+}
