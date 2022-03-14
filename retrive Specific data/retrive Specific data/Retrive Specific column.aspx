00cb59f3197cb3f781777a27676ea2aa8248bf7f
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-14 09:46:36 +0530

    Retrive Specific values

---
+++ retrive Specific data/retrive Specific data/Retrive Specific column.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Retrive Specific column.aspx.cs" Inherits="retrive_Specific_data.Retrive_Specific_column" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="label1data" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ retrive Specific data/retrive Specific data/Retrive Specific column.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace retrive_Specific_data
+{
+    public partial class Retrive_Specific_column : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string str = "data source=SAIKAT-SANIL\\SQL2019;initial catalog=B15;integrated security=true;";
+            SqlConnection conn = new SqlConnection(str);
+            string sqlstr = "select * from Employee1";
+            SqlCommand comd = new SqlCommand(sqlstr, conn);
+            SqlDataReader dr;
+            conn.Open();
+            label1data.Text = "Database Connected";
+            dr = comd.ExecuteReader();
+            while (dr.Read() == true)
+            {
+                label1data.Text = label1data.Text + " " + dr.GetValue(1) + " " + dr.GetValue(3) + " ";
+            }
+            dr.Close();
+            conn.Close();
+            label1data.Text = label1data.Text + " Database Disconnected";
+        }
+    }
+}
