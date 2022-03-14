0858307a2e66b7db184eceeb10091b16356a468f
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-14 17:30:41 +0530

    DataReader GridView

---
+++ DataReader Grid View/DataReader Grid View/Data Reader GridView.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Data Reader GridView.aspx.cs" Inherits="DataReader_Grid_View.Data_Reader_GridView" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DataReader Grid View/DataReader Grid View/Data Reader GridView.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DataReader_Grid_View
+{
+    public partial class Data_Reader_GridView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GetEmployee();
+        }
+        public void GetEmployee()
+        {
+            string str = "data source=SAIKAT-SANIL\\SQL2019;initial catalog=B15;integrated security=true";
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            string sqlstr = "SELECT * FROM Employee1";
+            SqlCommand comd = new SqlCommand(sqlstr, conn);
+            SqlDataReader dr;
+            dr = comd.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+        }
+    }
+}
