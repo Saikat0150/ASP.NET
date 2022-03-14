e8186620bfcf879132a038d6cb57d00fedfaae82
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-14 17:31:40 +0530

    Createing DataSet,nameSpace,Table

---
+++ Creating dataset, namespace, table/Creating dataset, namespace, table/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Creating_dataset__namespace__table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a Dataset,namespace and Student table
+            //Name and address column
+            DataSet ds = new DataSet("DS");
+            ds.Namespace = "StdNamespace";
+            DataTable stdTable = new DataTable("NewStudent");
+            DataColumn col1 = new DataColumn("Name");
+            DataColumn col2 = new DataColumn("Address");
+            stdTable.Columns.Add(col1);
+            stdTable.Columns.Add(col2);
+            ds.Tables.Add(stdTable);
+            //Add student Data to the table
+            DataRow newRow;
+            newRow = stdTable.NewRow();
+            newRow["Name"] = "Saikat";
+            newRow["Address"] = "Kolkata";
+            stdTable.Rows.Add(newRow);
+            newRow = stdTable.NewRow();
+            newRow["Name"] = "Anshuman";
+            newRow["Address"] = "Baruipur";
+            stdTable.Rows.Add(newRow);
+            newRow = stdTable.NewRow();
+            newRow["Name"] = "Preet";
+            newRow["Address"] = "Mumbai";
+            stdTable.Rows.Add(newRow);
+            ds.AcceptChanges();
+            //create a new streamWriter
+            //Save the date in some file Eg. stdData.xml
+            System.IO.StreamWriter myStreamWriter = new System.IO.StreamWriter(@"D:\stdData.xml");
+            //Write data to DataSet which actually creates the file
+            ds.WriteXml(myStreamWriter);
+            myStreamWriter.Close();
+            
+        }
+    }
+}
