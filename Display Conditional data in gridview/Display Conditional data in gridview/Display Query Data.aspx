508d578bf7cd617c7c305ac72385e7656192f96d
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-15 16:32:43 +0530

    Display Conditional Data by gridView

---
+++ Display Conditional data in gridview/Display Conditional data in gridview/Display Query Data.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Display Query Data.aspx.cs" Inherits="Display_Conditional_data_in_gridview.Display_Query_Data" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table align="center">
+            <tr>
+                <td>Name</td>
+                <td>
+                    <asp:TextBox ID="Name" runat="server"></asp:TextBox>
+
+                </td>
+                <td>
+                    <asp:Button ID="btn" runat="server" Text="Search" OnClick="btn_Click" />
+
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2"><asp:GridView ID="GridView1" runat="server"></asp:GridView></td>
+            </tr>
+         </table>
+    </form>
+</body>
+</html>
---
+++ Display Conditional data in gridview/Display Conditional data in gridview/Display Query Data.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Display_Conditional_data_in_gridview
+{
+    public partial class Display_Query_Data : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            string str = "data source=SAIKAT-SANIL\\SQL2019;initial catalog=B15;integrated security=true";
+            SqlConnection conn = new SqlConnection(str);
+            conn.Open();
+            string query = "SELECT * FROM form WHERE Name='" + Name.Text+"'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            GridView1.DataSource = dr;
+            GridView1.DataBind();
+        }
+    }
+}
