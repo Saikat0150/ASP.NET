30bcf5beedc4c15d18a66e3b0ee2a14397a9b568
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-17 14:54:51 +0530

    Cancel Edit Delete Insert without using Stored Procedure

---
+++ CancelUpdateDeleteEditWithoutStoredProcedure/CancelUpdateDeleteEditWithoutStoredProcedure/WebForm1.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CancelUpdateDeleteEditWithoutStoredProcedure.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table width ="40%" align="center" bgcoloe ="#cccccc">
+            <tr>
+                <td>Name</td>
+                <td>
+                    <asp:TextBox ID="name" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Mobile No: </td>
+                <td>
+                    <asp:TextBox ID="mob" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Age</td>
+                <td>
+                    <asp:TextBox ID="age" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Gender</td>
+                <td align="left">
+                    <asp:RadioButtonList ID="gender" runat="server" RepeatDirection="Horizontal">
+                        <asp:ListItem Value="M">Male</asp:ListItem>
+                        <asp:ListItem Value="F">Female</asp:ListItem>
+
+                    </asp:RadioButtonList>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align ="center" class="auto-style1">
+                    <asp:Button ID="Submit" runat="server" Text="Click To Register" OnClick="Submit_Click" />
+
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="Center">
+                    <asp:Label ID="Registered" runat="server" Text="Registration Status"></asp:Label>
+
+                </td>
+            </tr>
+
+        </table>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="ID" OnRowCancelingEdit="GridView1_RowCancelingEdit" OnRowDeleting="GridView1_RowDeleting" OnRowEditing="GridView1_RowEditing" OnRowUpdating="GridView1_RowUpdating">
+            <Columns>
+                <asp:TemplateField HeaderText="ID">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtID" runat="server" Text='<%# Bind("ID") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label1" runat="server" Text='<%# Bind("ID") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Name">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtName" runat="server" Text='<%# Bind("Name") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label2" runat="server" Text='<%# Bind("Name") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Mobile">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtMobile" runat="server" Text='<%# Bind("Mobile") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label3" runat="server" Text='<%# Bind("Mobile") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Age">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtAge" runat="server" Text='<%# Bind("Age") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label4" runat="server" Text='<%# Bind("Age") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="Gender">
+                    <EditItemTemplate>
+                        <asp:TextBox ID="txtGender" runat="server" Text='<%# Bind("Gender") %>'></asp:TextBox>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <asp:Label ID="Label5" runat="server" Text='<%# Bind("Gender") %>'></asp:Label>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:CommandField ShowEditButton="True" />
+                <asp:CommandField ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ CancelUpdateDeleteEditWithoutStoredProcedure/CancelUpdateDeleteEditWithoutStoredProcedure/WebForm1.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Web.UI.WebControls;
+
+namespace CancelUpdateDeleteEditWithoutStoredProcedure
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.BindGrid();
+            }
+        }
+        private void BindGrid()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            string query = "SELECT * FROM Employee2";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using(SqlDataAdapter sda=new SqlDataAdapter(query, con))
+                {
+                    using(DataTable dt=new DataTable())
+                    {
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            GridViewRow row = GridView1.Rows[e.RowIndex];
+            int ID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string Name = (row.FindControl("txtName") as TextBox).Text;
+            string Mobile = (row.FindControl("txtMobile") as TextBox).Text;
+            int Age = Convert.ToInt32((row.FindControl("txtAge") as TextBox).Text);
+            string Gender = (row.FindControl("txtGender") as TextBox).Text;
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            string query = "UPDATE Employee2 SET Name=@Name,Mobile=@Mobile,Age=@Age,Gender=@Gender WHERE ID=@ID";
+            using(SqlConnection con=new SqlConnection(constr))
+            {
+                using(SqlCommand cmd=new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@ID", ID);
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    cmd.Parameters.AddWithValue("@Mobile", Mobile);
+                    cmd.Parameters.AddWithValue("@Age", Age);
+                    cmd.Parameters.AddWithValue("@Gender", Gender);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.BindGrid();
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            int ID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            string query = "DELETE FROM Employee2 WHERE ID=@ID";
+            using(SqlConnection con=new SqlConnection(constr))
+            {
+                using(SqlCommand cmd=new SqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@ID", ID);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+            this.BindGrid();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            this.BindGrid();
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            string query = "Insert into Employee2 values (@0,@1,@2,@3)";
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("0", name.Text);
+                    cmd.Parameters.AddWithValue("1", mob.Text);
+                    cmd.Parameters.AddWithValue("2", age.Text);
+                    cmd.Parameters.AddWithValue("3", gender.SelectedValue);
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        Registered.Text = "Successfully Registered";
+                    }
+                    catch (Exception ex)
+                    {
+                        Registered.Text = "Not Registered Due to Invalid Input";
+                    }
+
+
+
+                    con.Close();
+
+                }
+            }
+            this.BindGrid();
+        }
+    }
+}
