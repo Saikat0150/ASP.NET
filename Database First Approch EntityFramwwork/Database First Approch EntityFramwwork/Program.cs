32295b284f1f7bed43864ec5cdbf5afc973ba38a
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-17 10:23:50 +0530

    Database First Approch EntityFrameWork

---
+++ Database First Approch EntityFramwwork/Database First Approch EntityFramwwork/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First_Approch_EntityFramwwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var cont=new Learn_EntityFrameworkEntities())
+            {
+                var result = cont.Products;
+                foreach(var item in result)
+                {
+                    Console.WriteLine("Product ID: " + item.Id);
+                    Console.WriteLine("Product Name: " + item.Name);
+                    Console.WriteLine("Product Price: " + item.Price);
+                    Console.WriteLine("Product Quantity: " + item.Quantity);
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
