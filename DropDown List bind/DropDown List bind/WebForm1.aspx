2d6a03009483d270662966f2129a7880cf1b8581
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-23 09:38:08 +0530

    dataBase First Approch

---
+++ DropDown List bind/DropDown List bind/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropDown_List_bind.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:DropDownList ID="DropDownList1" runat="server" AppendDataBoundItems="true">
+                <asp:ListItem Text="Select One" Value="Select One" Selected="True"></asp:ListItem>
+            </asp:DropDownList>
+            <asp:Button ID="btn" runat="server" Text="Search" OnClick="btn_Click" />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="Roll" HeaderText="Roll" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Phone" HeaderText="Phone" Visible="false" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" />
+                    <asp:BoundField DataField="Age" HeaderText="Age" />
+                    <asp:BoundField DataField="Gender" HeaderText="Gender" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ DropDown List bind/DropDown List bind/WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+
+namespace DropDown_List_bind
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                this.dropDown();
+            }
+        }
+        private void dropDown()
+        {
+            SqlConnection conn = new SqlConnection(constr);
+            string query = "SELECT Name FROM form";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            DropDownList1.DataSource = dt;
+            DropDownList1.DataTextField = "Name";
+            DropDownList1.DataValueField = "Name";
+            DropDownList1.DataBind();
+            conn.Close();
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(constr);
+            string query = "SELECT * FROM form WHERE Name = '" + DropDownList1.SelectedValue + "'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+    }
+}
