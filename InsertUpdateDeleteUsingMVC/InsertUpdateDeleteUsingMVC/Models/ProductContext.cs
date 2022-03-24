6e0170699bd8acb8c33504c5fa19ee8f8aa7dcca
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-24 16:48:52 +0530

    insert update delete using MVC

---
+++ InsertUpdateDeleteUsingMVC/InsertUpdateDeleteUsingMVC/Models/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace InsertUpdateDeleteUsingMVC.Models
+{
+    public class ProductContext:DbContext
+    {
+        public DbSet<Product> products { get; set; }
+    }
+}
