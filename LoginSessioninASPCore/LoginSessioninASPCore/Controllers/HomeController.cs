fb3648f380ca1e43056e0f67cf977a4da5867c8a
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-24 12:44:01 +0530

    LogInSessionUsing Asp core

---
+++ LoginSessioninASPCore/LoginSessioninASPCore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoginSessioninASPCore.Controllers
+{
+    [Route("home")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
