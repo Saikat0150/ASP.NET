ed6be29c4e6560c1d6a082617fa3ab62b64556ca
Saikat0150 <saikat.sanil.0150@gmail.com>
2022-03-24 12:45:17 +0530

    CreateDataToVBMVCCore

---
+++ CreateDatatoDBMVCCore/CreateDatatoDBMVCCore/Models/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateDatatoDBMVCCore.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+    }
+}
+
---
+++ CreateDatatoDBMVCCore/CreateDatatoDBMVCCore/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateDatatoDBMVCCore.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool Status { get; set; }
+    }
+
+}
+
